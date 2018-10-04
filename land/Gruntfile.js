7ba84fde7ceca7f8c1cf80af521e4bc2c69d0195
Raitis Berzins <raitisbe@gmail.com>
2018-10-04 12:37:28 +0300

    Fix usage of extended translatables.js file instead of global one

--- land/Gruntfile.js
+++ land/Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
         nggettext_compile: {
             databio: {
                 files: {
-                    'land/translations.js': ["land/merged.po"]
+                    'land/translations_extended.js': ["land/merged.po"]
                 }
             },
         },
